e472c74b22f72ff632c0d688d17b40accc3aba0d
esotericpig <esotericpig@users.noreply.github.com>
2024-08-19 12:11:48 -1000

    Add rand_double/float/int()

--- src/core/util/rando.cpp
+++ src/core/util/rando.cpp
@@ -16,6 +16,26 @@ Rando& Rando::it() {
   return it_;
 }
 
-bool Rando::rand_bool() { return bool_distr_(generator_); }
+bool Rando::rand_bool() { return bool_distr_(gen_); }
+
+double Rando::rand_double() { return double_distr_(gen_); }
+
+double Rando::rand_double(double min,double max) {
+  return double_distr_(gen_,std::uniform_real_distribution<double>::param_type(min,max));
+}
+
+float Rando::rand_float() { return float_distr_(gen_); }
+
+float Rando::rand_float(float min,float max) {
+  return float_distr_(gen_,std::uniform_real_distribution<float>::param_type(min,max));
+}
+
+int Rando::rand_int() { return int_distr_(gen_); }
+
+int Rando::rand_int(int max) { return rand_int(0,max); }
+
+int Rando::rand_int(int min,int max) {
+  return int_distr_(gen_,std::uniform_int_distribution<int>::param_type(min,max - 1));
+}
 
 } // Namespace.
--- src/core/util/rando.h
+++ src/core/util/rando.h
@@ -15,7 +15,12 @@
 namespace cybel {
 
 /**
- * Min & Max are always inclusive.
+ * Min is always inclusive.
+ * Max is always exclusive.
+ *
+ * Internally, this class passes the min/max to the distribution function.
+ * In my benchmark tests (on my system) using chrono, there was no increased time difference doing this,
+ * as opposed to using the constructor only, with a loop of 10 million.
  *
  * Rambo?
  */
@@ -31,10 +36,33 @@ public:
 
   bool rand_bool();
 
+  /**
+   * Between 0.0 & 1.0 (exclusive);
+   */
+  double rand_double();
+  double rand_double(double min,double max);
+
+  /**
+   * Between 0.0f & 1.0f (exclusive).
+   */
+  float rand_float();
+  float rand_float(float min,float max);
+
+  /**
+   * Between 0 & 100 (exclusive).
+   */
+  int rand_int();
+  int rand_int(int max);
+  int rand_int(int min,int max);
+
 private:
-  std::random_device device_{};
-  std::mt19937 generator_{device_()};
+  std::random_device dev_{};
+  std::mt19937 gen_{dev_()};
+
   std::bernoulli_distribution bool_distr_{0.5};
+  std::uniform_real_distribution<double> double_distr_{0.0,1.0};
+  std::uniform_real_distribution<float> float_distr_{0.0f,1.0f};
+  std::uniform_int_distribution<int> int_distr_{0,99}; // I don't have 99 problems.
 
   Rando();
 };
