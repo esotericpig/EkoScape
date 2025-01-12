a76c16be65bdeec3201f3796f92edb638fc0bff3
esotericpig <esotericpig@users.noreply.github.com>
2025-01-11 23:19:45 -1000

    Add Linux launcher script; fix itch manifest

---
+++ res/launcher.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cd "$(dirname "$0")"
+
+EKO_ARCH="$(uname -m)"
+EKO_BIN_ARCH='x86_64' # Default if arch not recognized.
+
+case "$EKO_ARCH" in
+'x86_64'|'x64')
+  EKO_BIN_ARCH='x86_64'
+  ;;
+'i386'|'i686'|'x86')
+  EKO_BIN_ARCH='i386'
+  ;;
+'aarch64'|'arm64')
+  EKO_BIN_ARCH='aarch64'
+  ;;
+'armhf'|'armv7l'|'arm')
+  EKO_BIN_ARCH='armhf'
+  ;;
+*)
+  echo "[WARN] Unknown arch [${EKO_ARCH}]; falling back to arch [${EKO_BIN_ARCH}]."
+  ;;
+esac
+
+EKO_BIN="./@BIN_NAME@-${EKO_BIN_ARCH}.AppImage"
+
+echo "Arch:      '${EKO_ARCH}'"
+echo "Bin arch:  '${EKO_BIN_ARCH}'"
+echo "Bin:       '${EKO_BIN}'"
+echo ''
+
+exec "$EKO_BIN" "$@"
