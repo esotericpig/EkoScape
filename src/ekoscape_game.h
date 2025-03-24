069d1f89aac76f404abc45e25d6149c07ec42db5
esotericpig <esotericpig@users.noreply.github.com>
2025-03-24 10:21:33 -1000

    Bump version to v2.3

--- src/ekoscape_game.h
+++ src/ekoscape_game.h
@@ -28,7 +28,7 @@ class EkoScapeGame : public Scene {
   std::unique_ptr<CybelEngine> cybel_engine_{};
 
 public:
-  static inline const std::string kTitle = "EkoScape v2.2";
+  static inline const std::string kTitle = "EkoScape v2.3";
 
   explicit EkoScapeGame();
 
